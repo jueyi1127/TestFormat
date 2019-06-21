11363edd3d4e8edc3f9490a77b6201f434b4fda1
Sandy Pan <sandy.pan@ringcentral.com>
2019-06-21 10:00:59 +0800

    Pre-action for Swift Format.

---
+++ TestFormat/Pods/Target Support Files/Pods-TestFormat/Pods-TestFormat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestFormat : NSObject
+@end
+@implementation PodsDummy_Pods_TestFormat
+@end
---
+++ TestFormat/TestFormat/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TestFormat
+//
+//  Created by Sandy Pan on 2019/6/21.
+//  Copyright © 2019 Sandy Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+func test() {
+        let isTest = false
+        if isTest {
+            NSLog("testing")
+        }
+        else{}
+    }
+}
